a92f7c762301259c66eb85c8108c7d422afc6138
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2023-01-19 14:13:50 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. #1 priority is stability. The first time it doesn’t run correctly we lose developer trust that this app is still relevant
+2. #2 priority is maintainability. Ties together with priority 1. Have to be able to easily edit this and update it to the latest version easily. else it’ll fall behind and no one will want to maintain it
+3. #3 Transparency. I don’t trust app engines. I always think they add BS I don’t need. I want this thing to spit out what it does different to a default rails app. when running the command it should list all the changes it made
---
+++ template.rb
@@ -0,0 +1,48 @@
+# See https://guides.rubyonrails.org/generators.html#application-templates
+
+git :init
+git add: "."
+git commit: "-a -m 'Initial commit'"
+
+gem_group :development, :test do
+
+  # Use RSpec for unittests instead of minitest
+  gem "rspec-rails"
+
+  # A library for generating fake data such as names, addresses, and phone numbers.
+  # https://github.com/faker-ruby/faker
+  gem "faker"
+
+  # Fixtures replacement with a straightforward definition syntax
+  # This will cause Rails to generate factories instead of fixtures.
+  # https://github.com/thoughtbot/factory_bot_rails/
+  gem "factory_bot_rails"
+
+  # Brakeman is a security scanner for Ruby on Rails applications.
+  # https://brakemanscanner.org/docs/introduction/
+  gem "brakeman"
+
+  # Standardize ruby formatting https://github.com/testdouble/standard
+  gem "standard"
+end
+
+# Generate needed boilerplate rspec configuration files
+rails_command "generate rspec:install"
+
+# Delete minitest specific test/ folder since we're using rspec's spec/ folder
+run "rm -rf test/"
+
+# Create factory bot configuration file
+create_file "spec/support/factory_bot.rb", <<~EOF
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+EOF
+
+# Auto fix common formatting violations of a greenfield rails app
+run "bundle exec standardrb --fix"
+
+after_bundle do
+  git add: '.'
+  git commit: "-a -m 'Generate binstubs, lint ruby'"
+end
